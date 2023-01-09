78d574c434942925602a6041affaf80495b3221f
MFurkan40 <mfurkan1140@gmail.com>
2023-01-09 19:35:09 +0300

    responsive done

--- src/pages/register/Register.style.jsx
+++ src/pages/register/Register.style.jsx
@@ -33,30 +33,37 @@ export const RegisterInput = styled(StyledInput)`
   height: 2.7rem;
   font-size: 1.5rem;
   width: 16rem;
-  margin: 0.8rem;
+  margin: 1.2rem;
 
   @media (max-width: ${({ theme }) => theme.screens.lg}) {
     height: 2.6rem;
     width: 14rem;
     font-size: 1.4rem;
+    margin: 1rem;
   }
 
   @media (max-width: ${({ theme }) => theme.screens.md}) {
     height: 2.5rem;
     width: 13rem;
     font-size: 1.3rem;
+    margin: 0.9rem;
   }
 
   @media (max-width: ${({ theme }) => theme.screens.sm}) {
     height: 2rem;
     width: 10rem;
+    margin: 0.8rem;
     font-size: 1.1rem;
   }
 `;
 
 export const RegisterButton = styled(StyledButton)`
-  margin: 0.6rem 1rem;
-  margin-bottom: 1.1rem;
+  margin: 1.3rem 1rem;
+
+  @media (max-width: ${({ theme }) => theme.screens.sm}) {
+    margin: 0.6rem 1rem;
+    margin-bottom: 1.1rem;
+  }
 `;
 
 export const SignInLink = styled(Link)`
